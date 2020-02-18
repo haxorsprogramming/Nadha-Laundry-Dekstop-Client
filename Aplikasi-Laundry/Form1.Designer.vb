ec445260ac0a1a93368cc047c44f539d22921671
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-19 05:33:41 +0700

    reef

--- Aplikasi-Laundry/Form1.Designer.vb
+++ Aplikasi-Laundry/Form1.Designer.vb
@@ -26,13 +26,15 @@ Partial Class Form1
         Me.btnKlik = New System.Windows.Forms.Button()
         Me.txtNama = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'btnKlik
         '
-        Me.btnKlik.Location = New System.Drawing.Point(207, 166)
+        Me.btnKlik.Location = New System.Drawing.Point(310, 255)
+        Me.btnKlik.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnKlik.Name = "btnKlik"
-        Me.btnKlik.Size = New System.Drawing.Size(75, 23)
+        Me.btnKlik.Size = New System.Drawing.Size(112, 35)
         Me.btnKlik.TabIndex = 0
         Me.btnKlik.Text = "Button1"
         Me.btnKlik.UseVisualStyleBackColor = True
@@ -40,33 +42,44 @@ Partial Class Form1
         'txtNama
         '
         Me.txtNama.AutoSize = True
-        Me.txtNama.Location = New System.Drawing.Point(215, 120)
+        Me.txtNama.Location = New System.Drawing.Point(66, 147)
+        Me.txtNama.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.txtNama.Name = "txtNama"
-        Me.txtNama.Size = New System.Drawing.Size(39, 13)
+        Me.txtNama.Size = New System.Drawing.Size(83, 20)
         Me.txtNama.TabIndex = 1
-        Me.txtNama.Text = "Label1"
+        Me.txtNama.Text = "Username"
         '
         'Button1
         '
         Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.Location = New System.Drawing.Point(438, 12)
+        Me.Button1.Location = New System.Drawing.Point(516, 14)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(39, 40)
+        Me.Button1.Size = New System.Drawing.Size(58, 62)
         Me.Button1.TabIndex = 2
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(70, 181)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(145, 26)
+        Me.TextBox1.TabIndex = 3
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(489, 328)
+        Me.ClientSize = New System.Drawing.Size(603, 434)
         Me.ControlBox = False
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.txtNama)
         Me.Controls.Add(Me.btnKlik)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Halaman Utama"
@@ -78,4 +91,5 @@ Partial Class Form1
     Friend WithEvents btnKlik As Button
     Friend WithEvents txtNama As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
