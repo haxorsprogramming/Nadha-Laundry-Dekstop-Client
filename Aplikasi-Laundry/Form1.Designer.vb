6fbc10deaaaeb7a8c21a64df031c05442a441cc7
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-19 05:29:11 +0700

    kali kedua

---
+++ Aplikasi-Laundry/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnKlik = New System.Windows.Forms.Button()
+        Me.txtNama = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnKlik
+        '
+        Me.btnKlik.Location = New System.Drawing.Point(207, 166)
+        Me.btnKlik.Name = "btnKlik"
+        Me.btnKlik.Size = New System.Drawing.Size(75, 23)
+        Me.btnKlik.TabIndex = 0
+        Me.btnKlik.Text = "Button1"
+        Me.btnKlik.UseVisualStyleBackColor = True
+        '
+        'txtNama
+        '
+        Me.txtNama.AutoSize = True
+        Me.txtNama.Location = New System.Drawing.Point(215, 120)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(39, 13)
+        Me.txtNama.TabIndex = 1
+        Me.txtNama.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(438, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(39, 40)
+        Me.Button1.TabIndex = 2
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 328)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.btnKlik)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Halaman Utama"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnKlik As Button
+    Friend WithEvents txtNama As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Aplikasi-Laundry/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnKlik.Click
+        txtNama.Text = "Halo"
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
